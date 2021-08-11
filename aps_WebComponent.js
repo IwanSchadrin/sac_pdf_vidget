7acbae9a25c1b6a6bffd67a0f2612056300cfcb9
IwanSchadrin <44634607+IwanSchadrin@users.noreply.github.com>
2021-08-11 11:29:04 +0200

    Update aps_WebComponent.js

--- aps_WebComponent.js
+++ aps_WebComponent.js
@@ -1,13 +1,17 @@
-(function() {
+(function () {
 	let template = document.createElement("template");
 	template.innerHTML = `
 		<form id="form">
 			<fieldset>
-				<legend>Custom Widget Text</legend>
+				<legend>PDF Widget Settings</legend>
 				<table>
 					<tr>
-						<td>Text</td>
-						<td><input id="aps_text" type="string"></td>
+						<td>PDF URL</td>
+						<td><input id="pdfUrl" type="string"></td>
+					</tr>
+					<tr>
+						<td>Pop-Up Title</td>
+						<td><input id="popupTitle" type="string"></td>
 					</tr>
 				</table>
 			</fieldset>
@@ -17,7 +21,7 @@
 	class PDFViewerAps extends HTMLElement {
 		constructor() {
 			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot = this.attachShadow({ mode: "open" });
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
 		}
@@ -25,22 +29,31 @@
 		_submit(e) {
 			e.preventDefault();
 			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-							// widgetText: this.widgetText
-						}
+				detail: {
+					properties: {
+						pdfUrl: this.pdfUrl,
+						popupTitle: this.popupTitle
 					}
+				}
 			}));
 		}
 
-		// set widgetText(newText) {
-		// 	this._shadowRoot.getElementById("aps_text").value = newText;
-		// }
+		set pdfUrl(newPdfUrl) {
+			this._shadowRoot.getElementById("pdfUrl").value = newPdfUrl;
+		}
 
-		// get widgetText() {
-		// 	return this._shadowRoot.getElementById("aps_text").value;
-		// }
+		get pdfUrl() {
+			return this._shadowRoot.getElementById("pdfUrl").value;
+		}
+
+		set popupTitle(newPopupTitle) {
+			this._shadowRoot.getElementById("popupTitle").value = newPopupTitle;
+		}
+
+		get popupTitle() {
+			return this._shadowRoot.getElementById("popupTitle").value;
+		}
 	}
 
-customElements.define("com-openpromos-sac-pdf-vidget-aps", PDFViewerAps);
+	customElements.define("com-openpromos-sac-pdf-vidget-aps", PDFViewerAps);
 })();
